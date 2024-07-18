9c504b3940b732344f928b053ccf6c63c105bf0e
tsizehena223 <tsizehena223@gmail.com>
2024-07-18 22:29:56 +0300

    refactor

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,17 +3,14 @@ import Navbar from "../components/Navbar";
 import Projects from "../components/Projects";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import { useLocation } from "react-router-dom";
 
 const Dashboard = () => {
   const [activePage, setActivePage] = useState(0);
   const user = localStorage.getItem('userData');
   const userToken = localStorage.getItem('dieToken');
   const navigate = useNavigate();
-  const location = useLocation();
 
   useEffect(() => {
-    const path = location.pathname;
     if (!user || !userToken) {
       navigate('/login');
       toast.error('You are not connected');
