a371d1912efbb533e23505eceb901170f4f79436
tsizehena223 <tsizehena223@gmail.com>
2024-09-08 11:38:03 +0300

    update: Login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -61,9 +61,6 @@ const Login = () => {
                   <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                   <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                 </div>
-                <div className="text-white">
-                  <a href="/login" className="underline text-sm">Forgot password?</a>
-                </div>
                 <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   Log in
                 </button>
